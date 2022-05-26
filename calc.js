7f5a03738d92c364d38ebd86d99797d464c6c39b
river <rrivverr@protonmail.com>
2022-05-26 03:17:08 -0400

    Create basic layout for calculator

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 
 const numPad = document.querySelector('.numpad')
 
-
+console.log(numPad);
 function add(a, b){
     return a + b;
 }
@@ -25,13 +25,39 @@ function operate(opFunction, a, b){
 
 
 function createNumpad(){
-    
-    
-    for (let i = 0; i < 10; i++){
+         
+    numPad.style.gridTemplate = `repeat(3, 1fr) / repeat(3, 1fr)`;
 
+    for (let i = 0; i < 10; i++){
+        const numButton = document.createElement('button');
+        setAttributes(numButton, {'class' : 'number', 'id': `num${i}`});
+        numButton.textContent = i;
+        if (i == 0) {
+            numPad.appendChild(numButton)       
+        }
+        else {
+
+        numPad.insertBefore(numButton, numPad.lastChild);
+        }
     }
+    const enterKey = document.createElement('button');
+    setAttributes(enterKey, {'class' : 'operator', 'id': 'enter'});
+    enterKey.textContent = '=';
+    numPad.appendChild(enterKey);
+
+ 
 }
 
+createNumpad();
+
+// Helper functiodn
+// Set multiple attributes from one function call
+function setAttributes(element, attributes)
+{
+    Object.entries(attributes).forEach(([key, value]) => {
+        element.setAttribute(key, value)
+    });
+}
 
 console.log(add(10, 2));
 console.log(subtract(10,2));
