91c8e53d42fc9b0e1446d66827ed4da60289b6e6
river <rrivverr@protonmail.com>
2022-05-26 13:15:42 -0400

    Create working screen, refresh, clear functions

--- calc.js
+++ calc.js
@@ -2,17 +2,22 @@
 const numPad = document.querySelector('.numpad')
 const screen = document.querySelector('.screen')
 
+let digits = []
+
+
 let calculation = {
     num1 : 0,
     num2 : 0,
-    operator : add(),
-    total : operate(operator, num1, num2)
+    operator : ""
 }
 
 
-let displayNumber = 0;
 
-console.log(numPad);
+let displayNumber;
+
+
+
+//console.log(numPad);
 function add(a, b){
     return a + b;
 }
@@ -31,22 +36,36 @@ function divide(a, b){
 
 
 function operate(opFunction, a, b){
+    console.log(opFunction)
     return (opFunction(a, b))
 }
 
+
+// Update screen
 function refreshScreen(){
+
+    // Remove leading 0s if theye exist
+    if (digits[0] == 0 && digits[1]){
+        digits.shift();
+    }
+    displayNumber = digits.join('');
     screen.textContent = displayNumber;
 }
 
-refreshScreen();
+// Clear array and refresh screen
+function resetScreen(){
+    digits = []
+    refreshScreen()
+}
 
+// Generate DIV/Buttons for numpad
 function createNumpad(){
          
     numPad.style.gridTemplate = `repeat(3, 1fr) / repeat(3, 1fr)`;
 
     for (let i = 0; i < 10; i++){
         const numButton = document.createElement('button');
-        setAttributes(numButton, {'class' : 'number', 'id': `num${i}`});
+        setAttributes(numButton, {'class' : 'number', 'id': `num${i}`, 'data-num': i});
         numButton.textContent = i;
         if (i == 0) {
             numPad.appendChild(numButton)       
@@ -62,6 +81,55 @@ function createNumpad(){
 
 createNumpad();
 
+
+
+// Store value currently on the screen, along with operator pressed,
+// in object for future manipulation
+const operation = document.querySelectorAll('.operator');
+operation.forEach(operator => {
+    operator.addEventListener('click', () => {
+        calculation.num1 = parseInt(digits.join(""));
+        console.log(calculation.num1);
+
+        calculation.operator = `${operator.id}`;
+        console.log(calculation.operator);
+        console.log(calculation.operator);
+        digits = []
+        refreshScreen();
+        
+    })
+})
+
+
+const equals = document.querySelector('#equals');
+    equals.addEventListener('click', () => {
+        calculation.num2 = parseInt(digits.join(""));
+        console.log(calculation.num2);
+        console.log(operate(calculation.operator, calculation.num1, calculation.num2));
+    })
+
+
+// store as an array for easy manipulation and display
+const numberInput = document.querySelectorAll('.number');
+numberInput.forEach(num => {
+    num.addEventListener('click', () => {
+        
+        // Cap screen size
+        if (digits.length < 8){
+        digits.push(num.getAttribute('data-num'));
+        console.log(displayNumber)
+        refreshScreen();
+        }
+    })
+})
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+    console.log('click')
+    resetScreen();
+});
+
+
 // Helper functiodn
 // Set multiple attributes from one function call
 function setAttributes(element, attributes)
@@ -71,7 +139,10 @@ function setAttributes(element, attributes)
     });
 }
 
+
+/*
 console.log(add(10, 2));
 console.log(subtract(10,2));
 console.log(multiply(10,2));
 console.log(divide(10,2));
+*/
